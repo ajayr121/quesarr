76498ea47296674b6035347804d4d7921f1a9096
Ajay Rajak <rajakajay1111@gmail.com>
2024-10-04 18:56:28 +0530

    Add files via upload

---
+++ mergetwoarray.py
@@ -0,0 +1,19 @@
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+arr3 = [9, 10, 11, 12]
+arr4 = [13, 14, 15, 16]
+merged_array = arr1 + arr2 + arr3 + arr4 
+print("The first array is: ")
+print(arr1)
+
+print("The second array is: ")
+print(arr2)
+
+print("The third array is: ")
+print(arr3)
+
+print("The fourth array is: ")
+print(arr4)
+
+print("The merged array of the given arrays after performing merge operation: ")
+print(merged_array)
